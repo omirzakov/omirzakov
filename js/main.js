91c553ec442779429490cf0be49a29d9cd9343ba
Madiyar <huistowns@gmail.com>
2021-01-02 13:05:24 +0600

    доработки в стилях и модальное окно

--- js/main.js
+++ js/main.js
@@ -1,7 +1,9 @@
 const infoButtons = document.querySelectorAll(".info .info-item");
 const navBtn = document.querySelector(".navbar-fixed .title");
 const navLinks = document.querySelector(".navbar-links");
-
+const openBtn = document.getElementById("modal-btn");
+const closeBtn = document.getElementById("modal-close");
+const modal = document.querySelector(".modal-wrapper");
 
 function accordeon(item) {
     item.forEach(item => {
@@ -19,6 +21,16 @@ accordeon(infoButtons);
 navBtn.addEventListener("click", function () {
     navBtn.classList.toggle("open")
     navLinks.classList.toggle("active");
+})
+
+openBtn.addEventListener("click", function () {
+    modal.classList.add("modal-active");
+    document.body.style.overflowY = "hidden";
+})
+
+closeBtn.addEventListener("click", function () {
+    modal.classList.remove("modal-active");
+    document.body.style.overflowY = "scroll";
 })
 
 
